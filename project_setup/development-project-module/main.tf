831114c29972f465e78c2a5c19269a5d12ed57eb
Jason Brown <jasonbrown@fastmail.fm>
2020-03-14 15:03:05 +0100

    attempt to automatically create project, set up service account and key for the project, but it is not working

--- project_setup/development-project-module/main.tf
+++ project_setup/development-project-module/main.tf
@@ -1,12 +1,16 @@
 resource "random_string" "random" {
   length = 8
-  special = true
-  override_special = "/@£$"
+  special = false
+}
+
+locals {
+    project_id = format("%s-%s-%s-%s", var.environment_type, var.region, var.data_classification, lower(random_string.random.result))
+    service_account_id = format("service-account-%s", lower(random_string.random.result))
 }
 
 resource "google_project" "project" {
     name        = var.name
-    project_id  = format("%s-%s-%s-%s", var.environment_type, var.region, var.data_classification, lower(random_string.random.result))
+    project_id  = local.project_id
     folder_id   = google_folder.department.name
     # probably should use labels to identify the various types of environments
     # e.g., 
@@ -29,3 +33,23 @@ resource "google_folder" "department" {
     parent          = format("organizations/%s", var.organization_id)
 }
 
+resource "google_project_iam_member" "project" {
+  project = local.project_id
+  role    = "roles/editor"
+  member  = "user:brownjasonj@gmail.com"
+}
+
+resource "google_service_account" "service_account" {
+  account_id    = local.service_account_id
+  display_name  = "Service Account"
+  project = local.project_id
+}
+
+resource "google_service_account_key" "service_account_key" {
+  service_account_id = google_service_account.service_account.name
+}
+
+data "google_service_account_key" "service_account_key" {
+  name            = google_service_account_key.service_account_key.name
+  public_key_type = "TYPE_X509_PEM_FILE"
+}
